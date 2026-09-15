4f6cd3779ece94b166ad61dc5ed342ce940d977d
clarityosbaerbelwesterop-gif <clarityos.baerbelwesterop@gmail.com>
2026-09-15 15:28:04 +0200

    M3: add world time, validation and streaming-safe registry checks

--- Source/BloodstreamWorld/Public/Framework/BloodstreamWorldFoundationSubsystem.h
+++ Source/BloodstreamWorld/Public/Framework/BloodstreamWorldFoundationSubsystem.h
@@ -1,19 +1,18 @@
 #pragma once
 #include "CoreMinimal.h"
-#include "Subsystems/WorldSubsystem.h"
+#include "Subsystems/TickableWorldSubsystem.h"
 #include "World/BloodstreamWorldTime.h"
 #include "BloodstreamWorldFoundationSubsystem.generated.h"
 struct FBloodstreamRegionDefinition;
 
 UCLASS()
-class BLOODSTREAMWORLD_API UBloodstreamWorldFoundationSubsystem : public UWorldSubsystem {
+class BLOODSTREAMWORLD_API UBloodstreamWorldFoundationSubsystem : public UTickableWorldSubsystem {
  GENERATED_BODY()
 public:
  virtual void Initialize(FSubsystemCollectionBase& Collection) override;
  virtual void Deinitialize() override;
- virtual void Tick(float DeltaSeconds);
- bool IsTickable() const { return true; }
- TStatId GetStatId() const;
+ virtual void Tick(float DeltaSeconds) override;
+ virtual TStatId GetStatId() const override;
  const FBloodstreamRegionDefinition* FindRegion(const FVector& WorldPosition) const;
  const FBloodstreamWorldTime& GetWorldTime() const { return WorldTime; }
  void SetTimePaused(bool bPaused){WorldTime.bPaused=bPaused;}
